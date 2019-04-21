d491e477eec910265128b1ec5d433da183ea3090
Randall Reed, Jr <randallreedjr@gmail.com>
2019-04-21 18:03:43 -0400

    Display X when a square is clicked

--- src/square.js
+++ src/square.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
 export default class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button
+        className="square"
+        onClick={() => this.setState({value: 'X'})}
+      >
+        {this.state.value}
       </button>
     );
   }
