e409e99e28dd524f407998e6254c6f65fc9d99e0
Randall Reed, Jr <randallreedjr@gmail.com>
2019-04-21 18:22:05 -0400

    Make Square a pure component

--- src/square.js
+++ src/square.js
@@ -1,14 +1,24 @@
 import React from 'react';
 
-export default class Square extends React.Component {
-  render() {
-    return (
-      <button
-        className="square"
-        onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+// Controlled component version
+// export default class Square extends React.Component {
+//   render() {
+//     return (
+//       <button
+//         className="square"
+//         onClick={() => this.props.onClick()}
+//       >
+//         {this.props.value}
+//       </button>
+//     );
+//   }
+// }
+
+// Pure component version
+export default function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  )
 }
