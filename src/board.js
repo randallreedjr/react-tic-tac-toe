a0eaa5ffa45cb005a2e34428ec6e3978d99f6601
Randall Reed, Jr <randallreedjr@gmail.com>
2019-04-21 18:23:51 -0400

    Alternate X's and O's

--- src/board.js
+++ src/board.js
@@ -6,14 +6,18 @@ export default class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
   handleClick(i) {
     // Copy the squares array from state
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
